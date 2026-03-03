37f4cb40abd4b6b6c29ff0cc580fadf32c5c0557
mmohanmahe86-sp <mmohanmahe86@gmail.com>
2026-03-03 19:01:52 +0530

    jenkinsfile updated

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ REQUEST_COUNT = Counter('app_requests_total', 'Total App HTTP Requests')
 @app.route("/")
 def home():
     REQUEST_COUNT.inc()
-    return  "CI/CD Deployment is Successful!!!"
+    return  "CI/CD Deployment Successful!!!"
 
 @app.route("/metrics")
 def metrics():
